618d2614399cefddf5d666128071cb74d6f03b33
shivasinghal2000 <singhalshiva936@gmail.com>
2020-12-03 01:52:19 +0530

    Modified Checkout page

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,19 +1,27 @@
 import React from 'react'
 import './Checkout.css'
+import CheckoutProduct from './CheckoutProduct'
+import { useStateValue } from './StateProvider'
 import Subtotal from './Subtotal'
 
 function Checkout () {
+  const [{ basket }, dispatch] = useStateValue()
   return (
     <div className='checkout'>
       <div className='checkout_left'>
         <img className='checkout_ad' src='https://images-eu.ssl-images-amazon.com/images/G/31/img19/AmazonPay/PrimeRewards/LP_Revamp/PC_Header_Banner._CB468631809_.jpg' alt='' />
         <div>
           <h2 className='checkout_title'>Your Shopping Basket</h2>
-          {/* BasketItem */}
-          {/* BasketItem */}
-          {/* BasketItem */}
-          {/* BasketItem */}
-          {/* BasketItem */}
+          {basket.map(item => (
+            <CheckoutProduct
+              key={item.id}
+              id={item.id}
+              image={item.image}
+              title={item.title}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
         </div>
       </div>
       <div className='checkout_right'>
