624481bdeca6a3279cc9aea0f238428085104ddc
shivasinghal2000 <singhalshiva936@gmail.com>
2020-12-03 03:11:49 +0530

    Created login page

---
+++ src/Login.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import './Login.css'
+
+function Login () {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const signIn = e => {
+    e.preventDefault() // prevents page from being refresh
+    
+    // some firebase login code will come here
+  }
+  const register = e => {
+    e.preventDefault()
+    
+    // some firebase register code will come here
+  }
+  return (
+    <div className='login'>
+      <Link to='/'>
+        <img className='login_logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1280px-Amazon_logo.svg.png' />
+      </Link>
+      <div className='login_container'>
+        <h1>Sign-in</h1>
+        <form>
+          <h5>E-mail</h5>
+          <input type='text' vlaue={email} onChange={e => setEmail(e.target.value)} />
+          <h5>Password</h5>
+          <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
+          <button type='submit' onClick={signIn} className='login_signInButton'>Sign In</button>
+        </form>
+        <p>By signing-in you agree to Amazon's FAKE CLONE Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.</p>
+        <button onClick={register} className='login_registerButton'>Create your Amazon Account</button>
+      </div>
+    </div>
+  )
+}
+
+export default Login
